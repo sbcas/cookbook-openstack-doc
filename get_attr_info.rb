ad3ca48022ce403ea0439a365164ec59949a514d
Mark Vanderwiel <vanderwl@us.ibm.com>
2016-05-05 14:42:27 -0500

    Update get_attr_info.rb

--- get_attr_info.rb
+++ get_attr_info.rb
@@ -62,7 +62,9 @@ def convert_attr_to_md(filename)
       end
     when comment?(line)
       if comment_header?(line)
+        puts "cmthdr b4: #{line}"
         line = '**' + line[/([*=]+\s)(.*)(\s[*=]+)/,2] + '**'
+        puts "cmthdr af: #{line}"
       end  
       if comment_header?(line) && code_block
         output << "\n####"
